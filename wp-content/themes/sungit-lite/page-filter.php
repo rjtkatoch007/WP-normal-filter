f80c37dac20856247fef5d838993e5808f2c16ba
rajat <rajat.k@sphinxworldbiz.co>
2023-07-30 10:33:44 +0530

    Filter posts by custom taxonomy term with AJAX with pagination

---
+++ wp-content/themes/sungit-lite/page-filter.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Template Name: Filter Template
+ */
+
+get_header(); ?>
+
+<section class="sec-content blog-page-content">
+        <div class="container">
+            <div class="row">                
+                <div class="col-md-12">
+                    <main id="main" class="site-main clearfix">
+					<?php echo do_shortcode('［ajax_filter_posts per_page="1"］'); ?>
+                    </main>
+	            </div>
+	        </div>
+	    </div>
+	</section>
+<?php
+get_footer();
